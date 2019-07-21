917088acecb66fea06a6bcaed175574ded9a5f91
integerzero <steadyprogrammer@gmail.com>
2019-07-21 18:47:44 +0900

    Global Variables

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import {join} from "path";
 import express from "express";
 import socketIO from "socket.io";
 import logger from "morgan";
+import socketController from "./socketController";
+import events from "./events";
 
 const PORT = 4000;
 const app = express();
@@ -9,7 +11,7 @@ app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
 app.use(express.static(join(__dirname, "static")));
 app.use(logger("dev"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (req, res) => res.render("home", {events: JSON.stringify(events)}));
 
 const handleListening = () => console.log(`Server running: http://localhost:${PORT}`);
 
@@ -17,14 +19,4 @@ const server = app.listen(PORT, handleListening);
 
 const io = socketIO.listen(server);
 
-io.on("connection", socket => {
-    socket.on("newMessage", ({ message }) => {
-        socket.broadcast.emit("messageNotification", {
-            message, 
-            nickname:socket.nickname || "Anonymous"
-        });
-    });
-    socket.on("setNickname", ({ nickname }) => {
-        socket.nickname = nickname;
-    });
-});
+io.on("connection", socket => socketController(socket));
---
+++ src/socketController.js
@@ -0,0 +1,8 @@
+const socketController = (socket) => {
+    socket.on(events.setNickname, ({nickname}) => {
+        console.log(nickname);
+        socket.nickname = nickname;
+    });
+}
+
+export default socketController;
